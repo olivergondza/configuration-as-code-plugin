664f5f9ea885330169a072a2eaf41b964c6430cb
Joseph Petersen <josephp90@gmail.com>
2019-04-03 03:52:26 +0200

    more conditions

--- .travis/run.sh
+++ .travis/run.sh
@@ -7,7 +7,7 @@ MVN_SETTINGS="${SCRIPT_PATH}/settings.xml"
 
 if [ "$1" == "install" ]; then
   mvn clean install --settings "${MVN_SETTINGS}" -DskipTests=true -Dmaven.javadoc.skip=true -B -V -Djava.level=${JAVA_LEVEL} ${MVN_FLAG}
-elif [[ "${TRAVIS_BRANCH}" == "testRelease" ]]; then
+elif [[ "${TRAVIS_BRANCH}" == "testRelease" && "${SHIPPABLE}" == "true" && "$1" == "release" ]]; then
   if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[maven-release-plugin]"* ]]; then
     echo "Do not release commits created by maven release plugin"
   else
