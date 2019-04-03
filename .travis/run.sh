b9d0a4bc062670d16300f67926e389e54282842f
Joseph Petersen <josephp90@gmail.com>
2019-04-03 03:08:33 +0200

    test dry run

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,32 +1,24 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 MVN_SETTINGS="${SCRIPT_PATH}/settings.xml"
 
 if [[ "$1" == "install" ]]; then
-  mvn clean install --settings ${MVN_SETTINGS} -DskipTests=true -Dmaven.javadoc.skip=true -B -V -Djava.level=${JAVA_LEVEL} ${MVN_FLAG}
-  exit 0
-fi
-
-if [[ "${TRAVIS_BRANCH}" == "release" ]]; then
+  mvn clean install --settings "${MVN_SETTINGS}" -DskipTests=true -Dmaven.javadoc.skip=true -B -V -Djava.level=${JAVA_LEVEL} ${MVN_FLAG}
+elif [[ "${TRAVIS_BRANCH}" == "testRelease" ]]; then
   if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[maven-release-plugin]"* ]]; then
     echo "Do not release commits created by maven release plugin"
   else
     echo "Building release"
 
-    # travis checkout the commit as detached head (which is normally what we
-    # want) but maven release plugin does not like working in detached head
-    # mode. This might be a problem if other commits have already been pushed
-    # to the release branch, but in that case we will have problem anyway.
-    git checkout release
+    git checkout "${TRAVIS_BRANCH}"
+    git remote set-url origin git@github.com:jenkinsci/configuration-as-code-plugin.git
+    git pull origin master --ff-only
 
-    # we should always be at the same level as master and only use fast forward
-    git merge master --ff-only
-
-    mvn release:prepare release:perform --settings ${MVN_SETTINGS} -B -Darguments="--settings ${MVN_SETTINGS} -DdryRun" -DdryRun
+    mvn release:prepare release:perform --settings "${MVN_SETTINGS}" -B -Darguments="--settings ${MVN_SETTINGS} -DdryRun" -DdryRun
   fi
 else
-  mvn clean install --settings ${MVN_SETTINGS} -B -Djava.level=${JAVA_LEVEL} ${MVN_FLAG}
+  mvn clean install --settings "${MVN_SETTINGS}" -B -Djava.level=${JAVA_LEVEL} ${MVN_FLAG}
 fi
